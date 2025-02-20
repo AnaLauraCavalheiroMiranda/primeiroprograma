8737a11a96f1e8cd5cef4fba3e4367003c748462
AnaLauraCavalheiroMiranda <ana.cavalheiro.miranda@escola.pr.gov.br>
2025-02-20 12:08:46 -0300

    Add files via upload

---
+++ primeiro.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int melancia;
+    int banana;
+    int soma;
+    int subtracao;
+    int multiplicacao;
+    melancia = 28;
+    banana = 50;
+    soma = melancia + banana;
+    multiplicacao = melancia * banana;
+    subtracao = banana - melancia;
+    printf("o valor de melancia é %d \n", melancia);
+    printf("o valor de banana é %d \n", banana);
+    printf("a soma é %d \n", soma);
+    printf("a multiplicacao é %d \n", multiplicacao);
+    printf("a subtracao é %d", subtracao);
+
+    return 0;
+}
